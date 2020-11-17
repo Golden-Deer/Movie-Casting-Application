5d7e5f10fae95040739e74d512fc95aac4d9af78
derickx1 <derickwxie@gmail.com>
2020-11-17 12:41:40 -0800

    Basic frontend for ProjectList

--- src/routes/project/ProjectList.js
+++ src/routes/project/ProjectList.js
@@ -1,1 +1,33 @@
-// project management page
+import db from "../../base";
+import "../../App.js";
+import React from 'react';
+import { useHistory } from "react-router-dom";
+import "./ProjectList.css"
+
+const ProjectList = () => {
+    const history = useHistory();
+    return (
+        <>
+            <div>
+                <h1 id="my-projects">
+                    <a onClick={() => history.push("/")}>&lt; My Projects</a>
+                </h1>
+                <button id={"your-account"}>Your Account</button>
+            </div>
+            <div className="project-attributes">
+                Project Name
+            </div>
+            <div className="project-attributes">
+                Description
+            </div>
+            <div className="project-attributes">
+                Logistic
+            </div>
+            <div className="project-attributes">
+                Role
+            </div>
+        </>
+    )
+}
+
+export default ProjectList;
