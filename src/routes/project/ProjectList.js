ab5fc80e27cbfbb75b9161df6631ed0a6d217c04
Jarrett Cornelison <jcornel00@gmail.com>
2020-11-17 20:39:56 -0800

    Moved projects CSS to one main CSS file

--- src/routes/project/ProjectList.js
+++ src/routes/project/ProjectList.js
@@ -1,33 +1,25 @@
-import db from "../../base";
-import "../../App.js";
+import db from '../../base';
+import '../../App.js';
 import React from 'react';
-import { useHistory } from "react-router-dom";
-import "./ProjectList.css"
+import { useHistory } from 'react-router-dom';
+import '../../App.css';
 
 const ProjectList = () => {
-    const history = useHistory();
-    return (
-        <>
-            <div>
-                <h1 id="my-projects">
-                    <a onClick={() => history.push("/")}>&lt; My Projects</a>
-                </h1>
-                <button id={"your-account"}>Your Account</button>
-            </div>
-            <div className="project-attributes">
-                Project Name
-            </div>
-            <div className="project-attributes">
-                Description
-            </div>
-            <div className="project-attributes">
-                Logistic
-            </div>
-            <div className="project-attributes">
-                Role
-            </div>
-        </>
-    )
-}
+  const history = useHistory();
+  return (
+    <>
+      <div>
+        <h1 id='my-projects'>
+          <a onClick={() => history.push('/')}>&lt; My Projects</a>
+        </h1>
+        <button id={'your-account'}>Your Account</button>
+      </div>
+      <div className='project-attributes'>Project Name</div>
+      <div className='project-attributes'>Description</div>
+      <div className='project-attributes'>Logistic</div>
+      <div className='project-attributes'>Role</div>
+    </>
+  );
+};
 
 export default ProjectList;
