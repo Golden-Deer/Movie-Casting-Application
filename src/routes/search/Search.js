c232f2f5a4497aa22a0d6530a12f559ab3290e51
Danlin <jiangdl0129@gmail.com>
2020-11-20 11:01:59 +0800

    Search update

--- src/routes/search/Search.js
+++ src/routes/search/Search.js
@@ -2,14 +2,22 @@ import db from "../../base";
 import "../../App.js";
 import React from 'react';
 import { useHistory } from "react-router-dom";
+import logo from "../../images/logo.png";
 
 const Search = () => {
         const history = useHistory();
         return (
             <>
-                <h1>Search</h1>
-                <button onClick={() => history.push("/")}>Home</button>
-                <button onClick={() => history.push("/discover")}>Discover</button>
+
+                    <div className="s-navbar">
+                            <img src={logo} className="app-logo"/>
+                            <h1 onClick={() => history.push("/")}>My Project</h1>
+                            <button id={"your-account"}>Your Account</button>
+                    </div>
+
+                    <button onClick={() => history.push("/search")}>Search</button>
+                    <button onClick={() => history.push("/discover")}>Discover</button>
+                    <button onClick={() => db.auth().signOut()}>Sign Out</button>
             </>
         )
 }
