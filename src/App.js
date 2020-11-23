6320cbfee9b50e45a5e6d23ce95af508b8173233
Jarrett Cornelison <jcornel00@gmail.com>
2020-11-22 19:58:56 -0800

    Working navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import PrivateRoute from "./auth/PrivateRoute";
-import { AuthProvider } from "./auth/Auth";
-import Home from "./routes/Home";
-import Login from "./routes/Login";
-import SignUp from "./routes/SignUp";
+import PrivateRoute from './auth/PrivateRoute';
+import { AuthProvider } from './auth/Auth';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import SignUp from './routes/SignUp';
 import passwordRecovery from './routes/passwordRecovery';
-import Discover from "./routes/discover/Discover"
-import Search from "./routes/search/Search"
+import Discover from './routes/discover/Discover';
+import Search from './routes/search/Search';
+import Navbar from './routes/layout/Navbar';
 
 function App() {
   return (
     <AuthProvider>
-        <Router>
-            <div>
-                <Route exact path="/" component={Home} />
-                {/* <Route exact path="/login" component={Login} /> */}
-                <Route exact path="/signup" component={SignUp} />
-                <Route exact path='/passwordrecovery' component={passwordRecovery}/>
-                <Route exact path="/search" component={Search} />
-                <Route exact path="/discover" component={Discover} />
-            </div>
-        </Router>
-      </AuthProvider>
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path='/' component={Home} />
+          {/* <Route exact path="/login" component={Login} /> */}
+          <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/passwordrecovery' component={passwordRecovery} />
+          <Route exact path='/search' component={Search} />
+          <Route exact path='/discover' component={Discover} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
