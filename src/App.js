441c55db85d247714e359d13260ae74456c90e63
Brendan <brendanchoudfo@gmail.com>
2020-11-23 12:58:06 -0800

    Separated homepage logic into separate files for better handling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PrivateRoute from './auth/PrivateRoute';
 import { AuthProvider } from './auth/Auth';
 import Home from './routes/Home';
-import Login from './routes/Login';
-import SignUp from './routes/SignUp';
 import passwordRecovery from './routes/passwordRecoveryPopup';
 import Discover from './routes/discover/Discover';
 import Search from './routes/search/Search';
 import Navbar from './routes/layout/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -18,8 +18,6 @@ function App() {
         <div>
           <Navbar />
           <Route exact path='/' component={Home} />
-          {/* <Route exact path="/login" component={Login} /> */}
-          <Route exact path='/signup' component={SignUp} />
           <Route exact path='/passwordrecovery' component={passwordRecovery} />
           <Route exact path='/search' component={Search} />
           <Route exact path='/discover' component={Discover} />
