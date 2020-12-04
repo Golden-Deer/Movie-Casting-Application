e671e14474a720d49b593d3c804fc128c7f335ac
Jarrett Cornelison <jcornel00@gmail.com>
2020-12-03 22:36:39 -0800

    Redesigned About and Home page

--- src/routes/layout/Navbar.js
+++ src/routes/layout/Navbar.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import Account from '../Account';
-import {Navbar, Nav} from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap';
 
 export const Navbar1 = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="/">
-      <img alt='Golden Cast' className="d-inline-block align-top" 
-      width="45" height="45" src={logo} />
+    <Navbar bg='dark' variant='dark'>
+      <Navbar.Brand href='/'>
+        <img
+          alt='Golden Cast'
+          className='d-inline-block align-top'
+          width='45'
+          height='45'
+          src={logo}
+        />
       </Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Projects</Nav.Link>
-      <Nav.Link href="/about">About Us</Nav.Link>
-      <Nav.Link href="/discover">Discover</Nav.Link>
-    </Nav>
-    <Account/>
-  </Navbar>
+      <Nav className='mr-auto'>
+        <Nav.Link href='/about'>About Us</Nav.Link>
+        <Nav.Link href='/discover'>Discover</Nav.Link>
+        <Nav.Link href='/'>Projects</Nav.Link>
+      </Nav>
+      <Account />
+    </Navbar>
   );
 };
 
