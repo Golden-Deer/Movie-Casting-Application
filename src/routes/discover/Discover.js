7ec80c76a339d506b9ece79f6031abd54b20e405
Jarrett Cornelison <jcornel00@gmail.com>
2020-12-07 04:03:17 -0800

    More rewording

--- src/routes/discover/Discover.js
+++ src/routes/discover/Discover.js
@@ -41,7 +41,7 @@ const Discover = () => {
         </Row>
         <Row>
           <Col>
-            <h1>Popular Actors From Ages 0 to 25</h1>
+            <h1>Popular Actors Between Ages 0 to 30</h1>
           </Col>
           <Col>
             <ExploreActor tags={{ age: '0-25' }} numActor={25} />
@@ -49,7 +49,7 @@ const Discover = () => {
         </Row>
         <Row>
           <Col>
-            <h1>Popular Actors From Ages 50 to 100</h1>
+            <h1>Popular Actors Between Ages 30 to 100</h1>
           </Col>
           <Col>
             <ExploreActor tags={{ age: '50-100' }} numActor={25} />
@@ -57,7 +57,7 @@ const Discover = () => {
         </Row>
         <Row>
           <Col>
-            <h1>Tall Actors</h1>
+            <h1>Actors With Height Between 170 and 250 cm</h1>
           </Col>
           <Col>
             <ExploreActor tags={{ height: '170-250' }} numActor={25} />
@@ -65,10 +65,10 @@ const Discover = () => {
         </Row>
         <Row>
           <Col>
-            <h1>Short Actors</h1>
+            <h1>Actors With Height Between 50 and 170 cm</h1>
           </Col>
           <Col>
-            <ExploreActor tags={{ height: '100-170' }} numActor={25} />
+            <ExploreActor tags={{ height: '50-170' }} numActor={25} />
           </Col>
         </Row>
       </Container>
