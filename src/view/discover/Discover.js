fe803003474b65a3b1be30c23de582f688384b52
Jarrett Cornelison <jcornel00@gmail.com>
2020-12-12 23:49:22 -0800

    Reduced number of categories in Discover

--- src/view/discover/Discover.js
+++ src/view/discover/Discover.js
@@ -1,59 +1,59 @@
 import '../../App.js';
 import React from 'react';
 import ExploreActor from './ExploreActor';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Discover = () => {
-    return (
-        <div className='about-page'>
-            <Container>
-                <Row>
-                    <Col>
-                        <h1>Trending Actors</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <ExploreActor numActor={25}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h1>Popular Male Actors</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <ExploreActor tags={{gender: 'male'}} numActor={25}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h1>Popular Female Actors</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <ExploreActor tags={{gender: 'female'}} numActor={25}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h1>Popular Actors Between Ages 0 to 30</h1>
-                    </Col>
-                    <Col>
-                        <ExploreActor tags={{age: '0-25'}} numActor={25}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h1>Popular Actors Between Ages 30 to 100</h1>
-                    </Col>
-                    <Col>
-                        <ExploreActor tags={{age: '50-100'}} numActor={25}/>
-                    </Col>
-                </Row>
-                <Row>
+  return (
+    <div className='about-page'>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Trending Actors</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ExploreActor numActor={25} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Popular Male Actors</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ExploreActor tags={{ gender: 'male' }} numActor={25} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Popular Female Actors</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ExploreActor tags={{ gender: 'female' }} numActor={25} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Popular Actors Between Ages 0 to 30</h1>
+          </Col>
+          <Col>
+            <ExploreActor tags={{ age: '0-25' }} numActor={25} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Popular Actors Between Ages 50 to 100</h1>
+          </Col>
+          <Col>
+            <ExploreActor tags={{ age: '50-100' }} numActor={25} />
+          </Col>
+        </Row>
+        {/* <Row>
                     <Col>
                         <h1>Actors With Height Between 170 and 250 cm</h1>
                     </Col>
@@ -68,10 +68,10 @@ const Discover = () => {
                     <Col>
                         <ExploreActor tags={{height: '50-170'}} numActor={25}/>
                     </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+                </Row> */}
+      </Container>
+    </div>
+  );
 };
 
 export default Discover;
