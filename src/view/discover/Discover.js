01510daf01ff1fe58247f62e0bda73aabd7e2571
Zifan Zhang <ziz012@ucsd.edu>
2020-12-13 15:53:47 +0800

    faster discover

--- src/view/discover/Discover.js
+++ src/view/discover/Discover.js
@@ -39,34 +39,10 @@ const Discover = () => {
                 </Row>
                 <Row>
                     <Col>
-                        <h1>Popular Actors Between Ages 0 to 30</h1>
+                        <h1>Popular Young Actors</h1>
                     </Col>
                     <Col>
                         <ExploreActor tags={{age: '0-25'}} numActor={25}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h1>Popular Actors Between Ages 30 to 100</h1>
-                    </Col>
-                    <Col>
-                        <ExploreActor tags={{age: '50-100'}} numActor={25}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h1>Actors With Height Between 170 and 250 cm</h1>
-                    </Col>
-                    <Col>
-                        <ExploreActor tags={{height: '170-250'}} numActor={25}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h1>Actors With Height Between 50 and 170 cm</h1>
-                    </Col>
-                    <Col>
-                        <ExploreActor tags={{height: '50-170'}} numActor={25}/>
                     </Col>
                 </Row>
             </Container>
